9b27765bdb4b197a4aba78d01489d3f632bb47a2
Valentin <gm.tupota.valik@gmail.com>
2021-11-21 21:50:23 +0300

    update basic CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// @ts-ignore
+import Video from './videos/video1.mp4';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TEST DEPLOY
-        </a>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <video width="800px" height="800px" controls>
+            <source src={Video} type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
+        </video>
       </header>
     </div>
   );
